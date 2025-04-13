d531a2c00e876cb12769346b6b8ebf96a14cf054
Nazar <nazar31032009@gmail.com>
2025-04-13 15:27:19 +0300

    add MyThread.java

---
+++ src/app/MyThread.java
@@ -0,0 +1,16 @@
+package app;
+
+class MyThread extends  {
+
+    dataHandler;
+
+    public MyThread(String name, DataHandler dataHandler) {
+        super(name);
+        this.dataHandler = dataHandler;
+    }
+
+    public  run() {
+        dataHandler.getOutput();
+    }
+}
+
